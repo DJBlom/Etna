47f1bbb38655f6f829e2bd2c78ffd65b4d9f744c
DJBlom <dawidjblom@gmail.com>
2022-08-30 19:54:49 -0400

    Updated comments in the registers files. Both .h and .cpp

--- test/tests/registers/static_registers.h
+++ test/tests/registers/static_registers.h
@@ -1,16 +1,3 @@
-/**********************************************************
- * Contents	: Definitions of the register access functions.
- *
- * Author	: Dawid Blom.
- *
- * Date		: 30/08/2022.
- *
- * NOTE		:
- **********************************************************/
-
-
-
-
 #ifndef _REGISTERS_H
 #define _REGISTERS_H
 
@@ -19,7 +6,6 @@ namespace driver
 {
 	namespace registers
 	{
-		// Multiple register bit operations.
 		template<typename address_t, typename position_t>
 		static inline void 		set(address_t &, position_t			);
 
@@ -38,9 +24,7 @@ namespace driver
 		template<typename address_t>
 		static inline address_t 	get(address_t &						);
 
-		
-		
-		// Single register bit operations.
+			// Single register bit operations.
 		template<typename address_t, typename position_t>
 		static inline void 		enable_bit(address_t &, position_t	);	
 
