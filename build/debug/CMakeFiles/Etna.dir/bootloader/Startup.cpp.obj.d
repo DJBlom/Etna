90ab3f624ed750046e07ce0f4bfaf6c38bec5523
Dawid Blom <dawidjblom@gmail.com>
2022-12-08 18:24:40 -0500

    Initial develop

--- build/debug/CMakeFiles/Etna.dir/bootloader/Startup.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Etna.dir/bootloader/Startup.cpp.obj: \
- /home/odin/workspace/c++_projects/embedded/etna/bootloader/Startup.cpp
--- build/debug/CMakeFiles/Etna.dir/source/src/components/GreenUserLED.cpp.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/Etna.dir/source/src/components/GreenUserLED.cpp.obj: \
- /home/odin/workspace/c++_projects/embedded/etna/source/src/components/GreenUserLED.cpp \
- /home/odin/workspace/c++_projects/embedded/etna/source/concrete/components/GreenUserLED.h \
- /home/odin/workspace/c++_projects/embedded/etna/source/abstract/components/LEDComponent.h
-/home/odin/workspace/c++_projects/embedded/etna/source/concrete/components/GreenUserLED.h:
-/home/odin/workspace/c++_projects/embedded/etna/source/abstract/components/LEDComponent.h:
