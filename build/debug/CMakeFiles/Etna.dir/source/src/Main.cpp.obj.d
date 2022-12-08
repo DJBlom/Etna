a04b64a3cdd4330db91c26cdf6f58520a1f804f3
Dawid Blom <dawidjblom@gmail.com>
2022-12-08 18:33:44 -0500

    Initial main commit

---
+++ build/debug/CMakeFiles/Etna.dir/source/src/Main.cpp.obj.d
@@ -0,0 +1,8 @@
+CMakeFiles/Etna.dir/source/src/Main.cpp.obj: \
+ /home/odin/workspace/c++_projects/embedded/etna/source/src/Main.cpp \
+ /home/odin/workspace/c++_projects/embedded/etna/source/concrete/registers/RegisterAddresses.h \
+ /home/odin/workspace/c++_projects/embedded/etna/source/concrete/HAL/GreenUserLED.h \
+ /home/odin/workspace/c++_projects/embedded/etna/source/abstract/HAL/LED.h
+/home/odin/workspace/c++_projects/embedded/etna/source/concrete/registers/RegisterAddresses.h:
+/home/odin/workspace/c++_projects/embedded/etna/source/concrete/HAL/GreenUserLED.h:
+/home/odin/workspace/c++_projects/embedded/etna/source/abstract/HAL/LED.h:
---
+++ build/debug/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: /home/odin/workspace/c++_projects/embedded/etna
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "DEBUG")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/home/odin/workspace/c++_projects/embedded/etna/toolchain/bin/arm-none-eabi-objdump")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/odin/workspace/c++_projects/embedded/etna/build/debug/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
