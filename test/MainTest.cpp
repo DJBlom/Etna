62acb449af68b5768995fd118fb34f1e7e628f31
DJBlom <63865918+DJBlom@users.noreply.github.com>
2022-12-08 20:55:01 -0500

    Update MainTest.cpp

--- test/MainTest.cpp
+++ test/MainTest.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /********************************************************************************
  * Contents: Main function of CppUTest.
  *
@@ -8,18 +7,6 @@
  *
  * Note: Used by CppUTest to execute all the tests of the project.
  *******************************************************************************/
-=======
-/**********************************************************************************
- * Contents: Main function for the CppUTest framework.
- *
- * Author: Dawid Blom.
- *
- * Data: December 8, 2022.
- *
- * NOTE: This file contains the main function to used by CppUTest to execute
- *       all the tests of the project.
- *********************************************************************************/
->>>>>>> main
 #include "CppUTest/CommandLineTestRunner.h"
 
 int main(int argc, char **argv)
