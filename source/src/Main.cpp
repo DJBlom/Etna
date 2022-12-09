ccb0a2db9b9c63651e939df032aa6b61aaca072f
Dawid Blom <dawidjblom@gmail.com>
2022-12-08 20:01:57 -0500

    Initial main commit

--- source/src/Main.cpp
+++ source/src/Main.cpp
@@ -1,33 +1,17 @@
-
-#include <cstdint>
-#include "RegisterAddresses.h"
-#include "GreenUserLED.h"
-
-
-using generic_t = std::uint32_t;
-
-volatile generic_t* busClock = GetAddress(registers::rcc::AHB1_ENABLE);
-volatile generic_t* mode = GetAddress(registers::gpio::gpioa::GPIOA_MODER);
-volatile generic_t* odr = GetAddress(registers::gpio::gpioa::GPIOA_ODR);
+/**********************************************************************************
+ * Contents: Main function.
+ *
+ * Author: Dawid Blom.
+ *
+ * Data: 08/12/2022.
+ *
+ * NOTE: This file contains the main function of the Etna application.
+ *********************************************************************************/
 
 int main()
 {
-    HAL::GreenUserLED led{busClock, mode, odr};
-
-
-    generic_t bitPosition = (1U << 5);
     while (true)
     {
-        led.EnableLED(bitPosition);
-        for (int i = 0; i < 500000; i++)
-        {
 
-        }
-
-        led.DisableLED(bitPosition);
-        for (int i = 0; i < 500000; i++)
-        {
-
-        }
     }
 }
