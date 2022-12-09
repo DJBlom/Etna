8408c6223bb8b20f9134f199f94b55d3f0bf6555
Dawid Blom <dawidjblom@gmail.com>
2022-12-08 19:49:36 -0500

    Testing

--- build/CMakeFiles/Etna.dir/source/src/Main.cpp.obj.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/Etna.dir/source/src/Main.cpp.obj: \
- /home/odin/workspace/c++_projects/embedded/etna/source/src/Main.cpp \
- /home/odin/workspace/c++_projects/embedded/etna/source/concrete/registers/RegisterAddresses.h \
- /home/odin/workspace/c++_projects/embedded/etna/source/concrete/HAL/GreenUserLED.h \
- /home/odin/workspace/c++_projects/embedded/etna/source/abstract/HAL/LED.h
-/home/odin/workspace/c++_projects/embedded/etna/source/concrete/registers/RegisterAddresses.h:
-/home/odin/workspace/c++_projects/embedded/etna/source/concrete/HAL/GreenUserLED.h:
-/home/odin/workspace/c++_projects/embedded/etna/source/abstract/HAL/LED.h:
