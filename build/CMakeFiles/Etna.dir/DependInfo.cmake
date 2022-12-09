9cc2a6d104c38a069e9f28ebf7791b4faf9e513e
Dawid Blom <dawidjblom@gmail.com>
2022-12-08 19:45:08 -0500

    Testing

---
+++ build/CMakeFiles/Etna.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/odin/workspace/c++_projects/embedded/etna/source/src/HAL/GreenUserLED.cpp" "CMakeFiles/Etna.dir/source/src/HAL/GreenUserLED.cpp.obj" "gcc" "CMakeFiles/Etna.dir/source/src/HAL/GreenUserLED.cpp.obj.d"
+  "/home/odin/workspace/c++_projects/embedded/etna/source/src/Main.cpp" "CMakeFiles/Etna.dir/source/src/Main.cpp.obj" "gcc" "CMakeFiles/Etna.dir/source/src/Main.cpp.obj.d"
+  "/home/odin/workspace/c++_projects/embedded/etna/startup/Startup.cpp" "CMakeFiles/Etna.dir/startup/Startup.cpp.obj" "gcc" "CMakeFiles/Etna.dir/startup/Startup.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
