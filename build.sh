1ae3c5900d0bc079582b7398dee0f44ea9954e1c
Dawid Blom <dawidjblom@gmail.com>
2023-01-21 14:03:33 -0500

    Fixed Static Code Analysis

--- build.sh
+++ build.sh
@@ -10,20 +10,22 @@ TYPE=Release
 BUILD_DIR=$BUILD
 TESTING_DIR=Testing
 RESET=
-TEST=Test
+UNIT_TEST=Test
+STATIC_TEST=StaticAnalysis
 VERBOSE=
 OPENOCD=
 
 
 for arg; do
     case "$arg" in
-        --help|-h)    echo $USAGE; exit 0;;
-        -v|--verbose) VERBOSE='VERBOSE=1'  ;;
-        Release)      TYPE=Release; BUILD_DIR=$BUILD/Release ;;
-        Test)         TYPE=Test; BUILD_DIR=$BUILD ;;
-        Reset)        RESET=1 ;;
-        Openocd)      OPENOCD=1 ;;
-        *)            echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
+        --help|-h)              echo $USAGE; exit 0;;
+        -v|--verbose)           VERBOSE='VERBOSE=1'  ;;
+        Release)                TYPE=Release; BUILD_DIR=$BUILD/Release ;;
+        Test)                   TYPE=Test; BUILD_DIR=$BUILD ;;
+        StaticAnalysis)         TYPE=StaticAnalysis; BUILD_DIR=$BUILD/StaticCodeAnalysis ;;
+        Reset)                  RESET=1 ;;
+        Openocd)                OPENOCD=1 ;;
+        *)                      echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
     esac
 done
 
@@ -34,12 +36,16 @@ elif [[ -n $RESET && -d $BUILD_DIR  ]]
 then
     rm -rf $BUILD_DIR/*
     rm -rf $TESTING_DIR
-elif [ "$TYPE" = "$TEST" ]
+elif [ "$TYPE" = "$UNIT_TEST" ]
 then
     $CMAKE -S . -B $BUILD_DIR --warn-uninitialized -DBUILD_TEST:TYPE=ON 
     $CMAKE --build $BUILD_DIR 
+elif [ "$TYPE" = "$STATIC_TEST" ]
+then
+    $CMAKE -S . -B $BUILD_DIR --warn-uninitialized -DBUILD_STATIC:TYPE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    $CMAKE --build $BUILD_DIR 
 else
-    $CMAKE -S . -B $BUILD_DIR --warn-uninitialized -DBUILD_ETNA:TYPE=ON -DCMAKE_BUILD_TYPE=$TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=cmakeModules/EtnaToolchain.cmake 
+    $CMAKE -S . -B $BUILD_DIR --warn-uninitialized -DBUILD_ETNA:TYPE=ON -DCMAKE_BUILD_TYPE=$TYPE -DCMAKE_TOOLCHAIN_FILE=cmakeModules/EtnaToolchain.cmake 
     $CMAKE --build $BUILD_DIR 
 fi
 
--- cmakeModules/StaticCodeAnalysis.cmake
+++ cmakeModules/StaticCodeAnalysis.cmake
@@ -6,21 +6,21 @@ function(StaticCodeAnalysis)
         set(CPPCHECK_OPTIONS --enable=all --std=c++20 --platform=mips32 --project=compile_commands.json -istartup -itest -icmakeModules -ibuild -itoolchain)
         set(STATIC_ANALYSIS ${PROJECT_NAME}StaticAnalysis)
 
-        #add_executable(${STATIC_ANALYSIS}
-        #    ${CMAKE_SOURCE_DIR}/source/src/etna/Main.cpp
-        #    ${EtnaSourceFiles}
-        #)
-
-        add_custom_target(${STATIC_ANALYSIS}
-            COMMAND ${CPPCHECK_EXECUTABLE} ${CPPCHECK_OPTIONS} ${EtnaSourceFiles}
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            COMMENT "Performing Static Code Analysis"
+        add_executable(${STATIC_ANALYSIS}
+            ${CMAKE_SOURCE_DIR}/source/src/etna/Main.cpp
+            ${EtnaSourceFiles}
         )
+
+        #add_custom_target(${STATIC_ANALYSIS}
+        #    COMMAND ${CPPCHECK_EXECUTABLE} ${CPPCHECK_OPTIONS} ${EtnaSourceFiles}
+        #    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        #    COMMENT "Performing Static Code Analysis"
+        #)
 
         include_directories(${STATIC_ANALYSIS} PRIVATE
             ${EtnaHeaderFiles}
         )
 
-        #target_link_libraries(${STATIC_ANALYSIS} PRIVATE)
+        target_link_libraries(${STATIC_ANALYSIS} PRIVATE)
     endif()
 endfunction()
