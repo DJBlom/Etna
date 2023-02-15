54ea2748006d32a7d7c11c019ef202d34afa0056
DJBlom <63865918+DJBlom@users.noreply.github.com>
2023-02-14 19:10:54 -0500

    Update FlashCode.sh

--- scripts/FlashCode.sh
+++ scripts/FlashCode.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-ls build/Release/source/
-
-#openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg -c "init;reset halt;flash write_image erase build/Release/source/Etna.elf;reset run;shutdown"
+openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg -c "init;reset halt;flash write_image erase Etna.elf;reset run;shutdown"
