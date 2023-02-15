faf5c517569633847bbcc301979248905867773c
DJBlom <63865918+DJBlom@users.noreply.github.com>
2023-02-14 19:35:44 -0500

    Update FlashCode.sh

--- scripts/FlashCode.sh
+++ scripts/FlashCode.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg -c "init;reset halt;flash write_image erase Etna.elf;reset run;shutdown"
+echo "target extended-remote /dev/ttyUSB0" > gdb.init
+echo "monitor reset halt" >> gdb.init
+echo "load path/to/firmware.elf" >> gdb.init
+echo "monitor reset" >> gdb.init
+echo "quit" >> gdb.init
+gdb -x gdb.init
+
+
+#openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg -c "init;reset halt;flash write_image erase Etna.elf;reset run;shutdown"
